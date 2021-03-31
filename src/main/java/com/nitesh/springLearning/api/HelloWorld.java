950155420232abe7a80abd9693b4a58ef269d141
Nitesh Jain <nitesh.jain@iiitb.org>
2021-03-31 16:30:16 +0530

    resolving white page error

--- src/main/java/com/nitesh/springLearning/api/HelloWorld.java
+++ src/main/java/com/nitesh/springLearning/api/HelloWorld.java
@@ -1,13 +1,15 @@
 package com.nitesh.springLearning.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloWorld {
-
-    @RequestMapping(value = "/hello")
-    public String sayHello(){
-        return "Hello World";
+    @GetMapping
+    public String home(){
+        return "Application is under maintainence " + new Date();
     }
 }
